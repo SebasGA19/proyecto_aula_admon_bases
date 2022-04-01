766f166a82a5a4bb23d9967a071a438cf4b72490
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2022-03-31 21:37:06 -0500

    upddate nav bar, alquilar auto, styles

---
+++ alquilar_auto.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>Login and Registration Form in HTML | CodingNepal</title>
+      <link rel="stylesheet" href="css/style.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body>
+      <div class="wrapper">
+         <div class="title-text">
+            <div class="title login">
+               Formulario de alquiler de autos
+            </div>
+         </div>
+         <div class="form-container">
+            
+            <div class="form-inner">
+               <form action="#" class="login">
+                  <div class="field">
+                     <input type="text" placeholder="ID cliente" name="id_cliente" required>
+                  </div>
+                  <div class="field">
+                     <input type="text" placeholder="ID empleado" name="id_empleado" required>
+                  </div>
+                  <div class="field">
+                     <input type="text" placeholder="ID Sucursal" name="id_sucursal" required>
+                  </div>
+                  <div class="field">
+                     <input type="text" placeholder="ID Vehículo" name="id_vehiculo" required>
+                  </div>
+                  <div class="field">
+                     <input type="text" placeholder="Cantidad de semanas" name="numero_semanas" required>
+                  </div>
+                  <div class="field">
+                     <input type="text" placeholder="Cantidad de dias" name="numero_dias" required>
+                  </div>
+                  <div class="field btn">
+                     <div class="btn-layer"></div>
+                     <input type="submit" value="Alquilar">
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+      <script>
+         const loginText = document.querySelector(".title-text .login");
+         const loginForm = document.querySelector("form.login");
+         const loginBtn = document.querySelector("label.login");
+         const signupBtn = document.querySelector("label.signup");
+         const signupLink = document.querySelector("form .signup-link a");
+         signupBtn.onclick = (()=>{
+           loginForm.style.marginLeft = "-50%";
+           loginText.style.marginLeft = "-50%";
+         });
+         loginBtn.onclick = (()=>{
+           loginForm.style.marginLeft = "0%";
+           loginText.style.marginLeft = "0%";
+         });
+         signupLink.onclick = (()=>{
+           signupBtn.click();
+           return false;
+         });
+      </script>
+   </body>
+</html>
---
+++ nav_bar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#Alquilar autos">Alquilar autos</a>
+  <a href="#Ver autos disponibles">Ver autos disponibles</a>
+  <a href="#Historial de alquileres">Historial de alquileres</a>
+</div>
+
+
+
+</body>
+</html>
