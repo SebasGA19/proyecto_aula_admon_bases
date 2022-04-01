43f6e4d4b11f4e626ebf1474974fff8b01d2aebb
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2022-04-01 16:41:36 -0500

    New nav - bar

--- nav_bar.php
+++ nav_bar.php
@@ -8,20 +8,46 @@ if (isset($_SESSION["id"])) {
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style.css">
+	<meta charset="UTF-8">
+	<title>Responsive Navigation Bar</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	
 
-<div class="topnav">
-    <a class="active" href="/dashboard.php">Home</a>
-    <a href="/vehiculos.php">Ver autos disponibles</a>
-    <a href="/historial-alquileres.php">Historial de alquileres</a>
-    <a href="/logout.php">Logout</a>
-</div>
+	<div class="navbar">
+		<div class="inner_navbar">
+			<div class="logo">
+				<a href="#">Auto <span>Rental</span></a>
+			</div>
+			<div class="menu">
+				<ul>
+					<li><a href="/dashboard.php" class="active">Home</a></li>
+					<li><a href="/vehiculos.php">Ver autos disponibles</a></li>
+					<li><a href="/historial-alquileres.php">Historial alquileres</a></li>
+					<li><a href="/logout.php">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="hamburger">
+			<i class="fas fa-bars"></i>
+		</div>
+	</div>
 
+
+
+<script>
+	var hamburger = document.querySelector(".hamburger");
+	var menu = document.querySelector(".menu");
+
+	hamburger.addEventListener("click", function(){
+		menu.classList.toggle("active");
+	})
+</script>
 
 </body>
 </html>
